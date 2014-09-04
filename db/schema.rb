7970375374fb39d30a4dd5786d9e8590b154ac88
Jan Marciniak <j.marciniakqtempotrac.com>
2014-09-04 18:03:17 +0200

    finish signinout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825183025) do
+ActiveRecord::Schema.define(version: 20140904134320) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.string   "subject"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,8 +28,10 @@ ActiveRecord::Schema.define(version: 20140825183025) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation)}
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remeber_token) }
 
   it { should be_valid }
 
@@ -112,6 +113,10 @@ describe User do
       it { should have_content('Sign up') }
       it { should have_title(full_title('Sign up')) }
     end
-
+    
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  	end
 end
 end
