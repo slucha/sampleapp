a4923caa9282e7393c9cdff8ae9a9e5dca4a5553
Jan Marciniak <j.marciniakqtempotrac.com>
2014-09-06 19:39:04 +0200

    Finsih useredit and update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,12 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			#User sign in
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 
 		else
-			# Wrong credentials message
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
-	end
+		end
 	end
 
 	def destroy
